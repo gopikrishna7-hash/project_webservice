3d4245dfb80ad34b6631e9e7bc718ac3f40587fc
gopikrishna7-hash <gopikrishna474747@gmail.com>
2025-05-05 22:21:06 +0530

    Update app.js
    making route dynamic

--- app.js
+++ app.js
@@ -105,7 +105,8 @@ app.use((err,req,res,next)=>{
     
 });
 
-
-app.listen(8080,()=>{
-    console.log("server is listening to 8080 ");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
