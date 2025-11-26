5c499ecdb7e12ccc915bba9e711754048b20844f
Gopi krishna <gopikrishna474747@gmail.com>
2025-11-27 00:34:56 +0530

    Refactor server listening port configuration
    making home / dynamic

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ app.use((err,req,res,next)=>{
 });
 
 
-app.listen(8080,()=>{
-    console.log("server is listening to 8080 ");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
